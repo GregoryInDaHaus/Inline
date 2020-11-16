f5d7cc2f776c3db625f024c8b313f516e9b75b58
stepanov_gy <stepanov_gy@albank.ru>
2020-11-16 14:21:08 +0900

    Added comments in table.php

--- controller/table.php
+++ controller/table.php
@@ -3,8 +3,18 @@ include "../lib/PDOAdapter.php";
 include "../lib/MyLogger.php";
 class AgesTable
 {
+    /**
+     * @var objectClass
+     * @param AgesTable параметр принимающий объект класса PDOAdapter
+     * для дальнейшего использования в методах текущего класса при подключении к БД
+     */
     private $adapter;
-
+    
+    /**
+     * @uses PDOAdapter()
+     * @uses MyLogger() 
+     *  присваивает @see adapter объект класса @uses PDOAdapter()
+     */
     function __construct(){
         $dsn = 'mysql:host=localhost;dbname=inline';
         $username = 'root'; 
@@ -12,34 +22,47 @@ class AgesTable
         $errorLogger = new MyLogger('../logs/log_file.txt');
         $this->adapter = new PDOAdapter($dsn, $username, $password, $errorLogger);
     }
+
+    /**
+     * Метод осуществляет выборку из БД, всей таблицы inline.person
+     * @return array ассоциативный массив "столбец" => "значение" из таблицы inline.person
+     */
     public function allTable(){
-        // Вывести всю таблицу
         $sql = "select * from inline.person";
         $sth = $this->adapter->prepare($sql);
         $result = $this->adapter->selectPrepared($sth);
         return $result;
     }
 
+    /**
+     * Метод выбирает максимальный возраст из таблицы inline.person
+     * @return array массив вида "max_age" => "значение"
+     */
     public function maxAge(){
-        // Определить максимальный возраст
         $sql = file_get_contents('../model/max_age.sql');
         $sth = $this->adapter->prepare($sql);
         $result = $this->adapter->selectPrepared($sth);
         return $result;
     }
 
+    /**
+     * Метод находит в БД персону, у которой знавение в столбце 
+     * mother_id не задан и возраст меньше максимального
+     * Изменяет у найденной персоны возраст на максимальный.
+     * @return int 1 при успешном выполнении запроса в БД
+     */
     public function updateOne(){
-        // Найти любую персону, у которой mother_id не задан и возраст меньше максимального
-        // изменить у нее возраст на максимальный
         $sql = file_get_contents('../model/update_one.sql');
         $sth = $this->adapter->prepare($sql);
         $result = $this->adapter->executePrepared($sth);
         return $result;
     }
 
+    /**
+     * Выбирает из таблицы inline.person список всех персон с максимальными возрастом
+     * @return array ассоциативный массив персон с максимальным возратом
+     */
     public function maxAgeList(){
-        // Получить список персон максимального возраста (фамилия, имя). 
-        // Желательно НЕ ИСПОЛЬЗУЯ полученное на шаге 1 значение.
         $sql = file_get_contents('../model/max_age_list.sql');
         $sth = $this->adapter->prepare($sql);
         $result = $this->adapter->selectPrepared($sth);
@@ -48,12 +71,8 @@ class AgesTable
 }
 
 if( isset($_POST['agelist']) ){
-$agesTable = new AgesTable;
-$result = $agesTable->maxAgeList();
+    $agesTable = new AgesTable;
+    $result = $agesTable->maxAgeList();
 
-echo json_encode($result);
-
-/*echo '<pre>';
-print_r($result);
-echo '</pre>';*/
+    echo json_encode($result);
 }
